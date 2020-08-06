e3887d09ba08f36d009a59af9a350b4ffb929ba8
Melissa <melissakheying@gmail.com>
2020-08-06 10:35:25 -0500

    Create API and fetch data

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+
+
+export default {
+  async index() {
+    const res = await fetch(
+      `https://my-json-server.typicode.com/Claim-Academy-JS/characters/characters`
+
+    )
+  return await res.json()
+  },
+}
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form } from './Form'
+import api from 'api'
 
 export const Table = () => {
+const [people, setPeople] = useState([])
 
-
+useEffect(() => {
+  (async () => {
+    const data = await api.index()
+    setPeople(data)
+  })()
+}, [])
 
 
 
