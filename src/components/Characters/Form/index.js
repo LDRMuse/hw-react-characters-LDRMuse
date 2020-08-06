ed82309591dd8821904c36a145e4ff2b88e5bf6b
Melissa <melissakheying@gmail.com>
2020-08-06 11:39:39 -0500

    Remove last name from form

--- src/components/Characters/Form/index.js
+++ src/components/Characters/Form/index.js
@@ -7,12 +7,9 @@ export const Form = ({ handler }) => {
   return (
     <Fragment>
       <form onSubmit={handler}>
-        <label htmlFor="firstName"></label>
-        <input placeholder="First Name" type="text"
+        <input placeholder="First Name Only" type="text"
           id="firstName" />
-        <label htmlFor="lastName"></label><input placeholder="Last Name" type="text"
-          id="lastName" />
-        <label htmlFor="job"></label><input placeholder="Job Description" type="text"
+        <input placeholder="Job Description" type="text"
           id="email" />
         <button type="Submit">Add Character</button>
       </form>
